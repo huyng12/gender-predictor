b95f58559db73b63600c33ebff31cb12931b1e7d
Huy <touch@nguyenrk.com>
2021-11-06 12:18:06 +0700

    api: Add uuid for preventing name duplication

--- api/handlers/predict.py
+++ api/handlers/predict.py
@@ -1,3 +1,5 @@
+import uuid
+
 from flask import request
 
 
@@ -5,8 +7,17 @@ def handle_predict(predictors):
     def handler():
         body = request.get_json(force=True)
         names = body['names']
+
+        # Add unique ID for preventing name duplication
+        names_with_uuid = []
+        for name in names:
+            names_with_uuid.append({
+                'name': name,
+                'uuid': str(uuid.uuid4())
+            })
+
         result = []
         for predictor in predictors:
-            result = result + predictor.predict(names)
+            result = result + predictor.predict(names_with_uuid)
         return {"data": result}, 200
     return handler
--- api/predictor/predictor.py
+++ api/predictor/predictor.py
@@ -7,7 +7,8 @@ class Predictor:
         self.model = model
         self.model_name = model_name
 
-    def predict(self, names):
+    def predict(self, names_with_uuid):
+        names = [name_with_uuid['name'] for name_with_uuid in names_with_uuid]
         X = self.vectorizer.transform(names)
 
         # Predict genders and probabilities
@@ -16,10 +17,11 @@ class Predictor:
 
         # Transform result
         result = []
-        for index, name in enumerate(names):
+        for index, name_with_uuid in enumerate(names_with_uuid):
             gender, proba = genders[index], probas[index]
             result.append({
-                'name': name,
+                'name': name_with_uuid['name'],
+                'uuid': name_with_uuid['uuid'],
                 'gender': 'male' if gender == Gender.MALE.value else 'female',
                 'accuracy': proba[gender],
                 'model_name': self.model_name
