243b541d84ff134538a4a9e73aad19fcbd48dbe9
Huy <touch@nguyenrk.com>
2021-11-06 14:34:55 +0700

    api: Allow gender-predictor.nguyenrk.com in CORS

--- api/app.py
+++ api/app.py
@@ -1,3 +1,5 @@
+from flask_cors import CORS
+
 from handlers.errors.method_not_allowed import handle_method_not_allow
 from handlers.errors.not_found import handle_not_found
 from handlers.health_check import handle_health_check
@@ -5,6 +7,9 @@ from handlers.predict import handle_predict
 
 
 def setup_app(app, predictors):
+    # Register CORS
+    CORS(app, origins="https://gender-predictor.nguyenrk.com")
+
     # Register app routes
     app.add_url_rule('/', view_func=handle_health_check, methods=['GET'])
     app.add_url_rule(
